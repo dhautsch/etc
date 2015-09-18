dc54632345c0315297ac75042a66f9c744e9faa2
dhautsch <don@hautsch.com>
2015-09-18 12:02:54 -0400

    Changed line separator to 1.6

--- java/XmlPath.java
+++ java/XmlPath.java
@@ -5,8 +5,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.StringWriter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -18,6 +20,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
@@ -28,12 +31,11 @@ import org.w3c.dom.ls.LSSerializer;
 
 public class XmlPath {
 	private Document _xmlDocument = null;
+	private final static String _lineSeparatorProp = "line.separator";
 
 	/**
-	 * 
 	 * @param args
 	 */
-
 	public static void main(String[] args) {
 		XmlPath xmlPath_ = null;
 		boolean printUsage_ = false;
@@ -49,7 +51,6 @@ public class XmlPath {
 				xmlPath_ = new XmlPath();
 
 				xmlPath_.setXmlDocument(new File(args[1]));
-
 				al_ = xmlPath_.xpathToNodeList(args[2]);
 
 				for (int i_ = 0; i_ < al_.size(); i_++)
@@ -58,39 +59,40 @@ public class XmlPath {
 							out_ = "<DATA>";
 
 						out_ = out_.concat(nodeToXML(al_.get(i_)));
-
 					} else {
 						String tag_ = nodeToTag(al_.get(i_));
-						HashMap<String, String> attrMap_ = nodeToAttributes(al_.get(i_));
+						HashMap<String, String> attrMap_ = nodeToAttributes(al_
+								.get(i_));
 
 						if (al_.size() > 1)
 							tag_ = tag_.concat("[" + i_ + "]");
 
 						out_ = out_.concat(tag_ + "="
 								+ nodeToValue(al_.get(i_))
-								+ System.lineSeparator());
+								+ System.getProperty(_lineSeparatorProp));
 
 						for (String k_ : attrMap_.keySet())
-							out_ = out_.concat(tag_ + "." + k_ + "="
-							+ attrMap_.get(k_)
-							+ System.lineSeparator());
+							out_ = out_
+									.concat(tag_ + "." + k_ + "="
+											+ attrMap_.get(k_)
+											+ System.getProperty(_lineSeparatorProp));
 					}
 
 				if (isNotBlank(out_)) {
 					if (getXml_)
 						out_ = prettyPrintXml(out_.concat("</DATA>")
-								+ System.lineSeparator());
+								+ System.getProperty(_lineSeparatorProp));
 
 					System.out.print(out_);
 				}
 			}
-
 		} else {
 			printUsage_ = true;
 		}
 
 		if (printUsage_)
 			System.err.println("(getXML|getValue) filePath XPath");
+
 	}
 
 	public static boolean isNotBlank(String s) {
@@ -104,14 +106,12 @@ public class XmlPath {
 			DOMImplementationLS lsImpl = (DOMImplementationLS) n
 					.getOwnerDocument().getImplementation()
 					.getFeature("LS", "3.0");
-
 			LSSerializer serializer = lsImpl.createLSSerializer();
 
 			serializer.getDomConfig().setParameter("xml-declaration", false);
 
 			ret_ = serializer.writeToString(n);
 		}
-
 		return ret_;
 	}
 
@@ -144,12 +144,10 @@ public class XmlPath {
 
 				for (int j_ = 0; j_ < nnm_.getLength(); j_++) {
 					Attr attr_ = (Attr) nnm_.item(j_);
-
 					ret_.put(attr_.getNodeName(), attr_.getNodeValue());
 				}
 			}
 		}
-
 		return ret_;
 	}
 
@@ -162,10 +160,8 @@ public class XmlPath {
 				XPathExpression xPathExpr_;
 
 				xPathExpr_ = xPath_.compile(xpath);
-
 				NodeList nl_ = (NodeList) xPathExpr_.evaluate(doc,
 						XPathConstants.NODESET);
-
 				for (int i_ = 0; i_ < nl_.getLength(); i_++)
 					ret_.add(nl_.item(i_));
 
@@ -182,16 +178,13 @@ public class XmlPath {
 
 	public String xpathFirstNodeValue(String xpath) {
 		String ret_ = "";
-
 		ArrayList<Node> al_ = xpathToNodeList(getXmlDocument(), xpath);
 
 		if (al_.isEmpty() == false) {
 			ret_ = nodeToValue(al_.get(0));
-
 			if (isNotBlank(ret_) == false)
 				ret_ = "";
 		}
-
 		return ret_;
 	}
 
@@ -208,7 +201,6 @@ public class XmlPath {
 
 	public void setXmlDocument(File file) {
 		_xmlDocument = null;
-
 		if (file != null)
 			try {
 				setXmlDocument(new FileInputStream(file));
@@ -244,7 +236,6 @@ public class XmlPath {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
 		return ret_;
 	}
 
@@ -266,36 +257,26 @@ public class XmlPath {
 
 	public static boolean prettyPrintXml(Document doc, StreamResult sr) {
 		boolean ret_ = false;
-
 		if (doc != null && sr != null)
 			try {
 				TransformerFactory tf = TransformerFactory.newInstance();
 				Transformer transformer = tf.newTransformer();
 
-				transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-
+				transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION,
+						"no");
 				transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-
 				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-
 				transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-
 				/*
-				 * 
 				 * transformer.setOutputProperty(
-				 * 
-				 * "{http://xml.apache.org/xslt}indent-amount ", "4");
+				 * "{http://xml.apache.org/xslt}indent-amount", "4");
 				 */
-
 				// new OutputStreamWriter(out, "UTF-8")));
-
 				transformer.transform(new DOMSource(doc), sr);
-
 				ret_ = true;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
 		return ret_;
 	}
 
@@ -308,7 +289,6 @@ public class XmlPath {
 			if (prettyPrintXml(doc, new StreamResult(w_)))
 				ret_ = w_.getBuffer().toString();
 		}
-
 		return ret_;
 	}
 

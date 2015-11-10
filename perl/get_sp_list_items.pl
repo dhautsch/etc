4b17cbf396d485f22fc45f4782e69eb53636be33
dhautsch <don@hautsch.com>
2015-11-10 15:23:51 -0500

    updates

--- perl/get_sp_list_items.pl
+++ perl/get_sp_list_items.pl
@@ -8,7 +8,7 @@ use ETLSuppUtils;
 use strict;
 
 my %PROPS = readProps("$ENV{HOME}/etlsupp/config.properties");
-my $SHAREPOINT_CONN_PROP = 'com.fanniemae.a22.sharepoint_conn';
+my $SHAREPOINT_CONN_PROP = 'com.company.a22.sharepoint_conn';
 my $TMP_DIR = getTmpDirName();
 my $GET_LIST_RESPONSE_XML = "$TMP_DIR/GetListResponse.xml";
 my $HTML_HEADER = "'Accept: application/atom+xml; charset=utf-8'";
@@ -20,7 +20,7 @@ my $EXIT = 1;
 
 END { whackTmpDirList(); exit($EXIT) };
 
-$OPTS{help}++ unless GetOptions(\%OPTS, qw(help xml meta verbose example));
+$OPTS{help}++ unless GetOptions(\%OPTS, qw(help xml meta verbose query=s example));
 
 if ($OPTS{example}) {
 	while (<DATA>) {
@@ -32,10 +32,11 @@ if ($OPTS{example}) {
 $OPTS{help}++ unless @ARGV == 2;
 
 if ($OPTS{help}) {
-	print STDERR "Usage : $0 [-help -meta -verbose -xml -example] <URL> <LIST_TITLE>\n";
+	print STDERR "Usage : $0 [-help -meta -verbose -query QUERY -xml -example] <URL> <LIST_TITLE>\n";
 	print STDERR "\t-help : print this message.\n";
 	print STDERR "\t-meta : print list metadata instead of items.\n";
 	print STDERR "\t-verbose : print curl output to STDERR.\n";
+	print STDERR "\t-query QUERY : GET query parameter.\n";
 	print STDERR "\t-xml : output xml.\n";
 	print STDERR "\t-example : show example code.\n";
 	exit($EXIT);
@@ -43,9 +44,30 @@ if ($OPTS{help}) {
 
 $ARGV[1] =~ s! !%20!g;
 
-my $URL = "'$ARGV[0]/_api/lists/getbytitle%28%27$ARGV[1]%27%29";
+my $URL = "'$ARGV[0]/_api/lists/getByTitle%28%27$ARGV[1]%27%29";
 
 $URL .= '/items' unless $OPTS{meta};
+
+if ($OPTS{query}) {
+	$OPTS{query} =~ s!\ !%20!g;
+	$OPTS{query} =~ s!\!!%21!g;
+	$OPTS{query} =~ s!\"!%22!g;
+	$OPTS{query} =~ s!\#!%23!g;
+	$OPTS{query} =~ s!\$!%24!g;
+	$OPTS{query} =~ s!\&!%26!g;
+	$OPTS{query} =~ s!\'!%27!g;
+	$OPTS{query} =~ s!\(!%28!g;
+	$OPTS{query} =~ s!\)!%29!g;
+	$OPTS{query} =~ s!\*!%2A!g;
+	$OPTS{query} =~ s!\;!%3B!g;
+	$OPTS{query} =~ s!\<!%3C!g;
+	$OPTS{query} =~ s!\>!%3E!g;
+	$OPTS{query} =~ s!\?!%3F!g;
+
+	$URL .= '?';
+	$URL .= $OPTS{query};
+}
+
 $URL .= "'";
 
 if ($URL =~ m!http://([^:/]+)!) {
@@ -162,7 +184,7 @@ print Dumper(\@array_); # Should see error message
 @array_ = getMeta(qw(http://sharepoint/eso-sites/etlinfraeng/etl Tasks));
 print Dumper(\@array_);
 
-@array_ = getItems(qw(http://sharepoint/eso-sites/etlinfraeng/etl Tasks));
+@array_ = getItems(qw(http://sharepoint/eso-sites/etlinfraeng/etl Tasks $top=500));
 print Dumper(\@array_);
 
 sub getMeta {
@@ -184,10 +206,15 @@ sub getMeta {
 sub getItems {
 	my $url_ = shift;
 	my $list_ = shift;
+	my $query_ = shift;
 	my @items_;
 
 	if ($url_ && $list_) {
-		my $VAR1 = qx(./get_sp_list_items.pl $url_ $list_);
+		if ($query_) {
+			$query_ .= "-query '$query_'";
+		}
+
+		my $VAR1 = qx(./get_sp_list_items.pl $query_ $url_ $list_);
 
 		if ($VAR1) {
 			eval $VAR1;
@@ -196,4 +223,3 @@ sub getItems {
 	}
 	return @items_;
 }
-

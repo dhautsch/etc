13af8432b686ec178729c42dbb8ac6acec1419b1
dhautsch <don@hautsch.com>
2016-12-01 12:57:54 -0500

    Create snippets.py

---
+++ jython/snippets.py
@@ -0,0 +1,21 @@
+def qx(cmd):
+        pipe_ = subprocess.Popen(cmd, stdout=PIPE, shell=True, bufsize=-1, close_fds=True)
+        try:
+                for l_ in pipe_.stdout:
+                        s_ = l_.rstrip()
+                        if len(s_) > 0: yield(s_)
+        finally:
+                pipe_.stdout.close()
+
+HOST = ''.join(qx('uname -n'))
+
+BANNER = [s_ for s_ in """
+DATE
+DEBUG
+ERROR
+INFO
+WARN
+LOG
+""".split("\n") if s_]
+
+LEVELS = [s_ for s_ in BANNER if s_ != 'DATE' and s_ != 'LOG']

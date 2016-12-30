5d6278a263918fafb9a5e596e0360aa1af9b03a7
dhautsch <don@hautsch.com>
2016-12-30 18:18:49 -0500

    Update snippets.py

--- jython/snippets.py
+++ jython/snippets.py
@@ -44,3 +44,26 @@ logging.warning('this is just an eample.')
 s = subprocess.call(["ls", "-ltr"])
 s=os.system('echo $PWD')
 print("s : %s" % (s))
+
+
+from wsgiref.simple_server import make_server
+
+# Every WSGI application must have an application object - a callable
+# object that accepts two arguments. For that purpose, we're going to
+# use a function (note that you're not limited to a function, you can
+# use a class for example). The first argument passed to the function
+# is a dictionary containing CGI-style envrironment variables and the
+# second variable is the callable object (see :pep:`333`)
+def hello_world_app(environ, start_response):
+    status = '200 OK' # HTTP Status
+    headers = [('Content-type', 'text/plain')] # HTTP Headers
+    start_response(status, headers)
+
+    # The returned object is going to be printed
+    return ["Hello World"]
+
+httpd = make_server('', 8000, hello_world_app)
+print "Serving on port 8000..."
+
+# Serve until process is killed
+httpd.serve_forever()

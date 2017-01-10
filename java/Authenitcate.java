eeb6c533175177c1a8e4e35e415d468aec72729d
dhautsch <don@hautsch.com>
2017-01-10 14:41:42 -0500

    Update Authenitcate.java

--- java/Authenitcate.java
+++ java/Authenitcate.java
@@ -1,5 +1,6 @@
 import java.util.Hashtable;
 import javax.naming.Context;
+import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 
@@ -14,18 +15,22 @@ public class Authenitcate {
 			System.out.println("Usage: Authenticate PROVIDER_URL SECURITY_PRINCIPLE PASSWORD_ENV_VAR");
 			System.exit(1);
 		}
-		Hashtable<String,Object> env = new Hashtable<String,Object>();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-		env.put(Context.SECURITY_AUTHENTICATION, "simple");
-		env.put(Context.PROVIDER_URL, args[0]);
-		env.put(Context.SECURITY_PRINCIPAL, args[1]);
-		env.put(Context.SECURITY_CREDENTIALS, System.getenv(args[2]));
+		DirContext ctx_ = null;
+		Hashtable<String,Object> env_ = new Hashtable<String,Object>();
+		env_.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env_.put(Context.SECURITY_AUTHENTICATION, "simple");
+		env_.put(Context.PROVIDER_URL, args[0]);
+		env_.put(Context.SECURITY_PRINCIPAL, args[1]);
+		env_.put(Context.SECURITY_CREDENTIALS, System.getenv(args[2]));
 		try {
-			DirContext ctx = new InitialDirContext(env);
-			System.out.println("LOGIN SUCCESSFUL " + ctx.getAttributes(env.get(Context.SECURITY_PRINCIPAL).toString()));
+			ctx_ = new InitialDirContext(env_);
+			System.out.println("LOGIN SUCCESSFUL " + ctx_.getAttributes(env_.get(Context.SECURITY_PRINCIPAL).toString()));
 			System.exit(0);
 		} catch (Exception e) {
 			System.out.println(e);
+		}
+		finally {
+			if (ctx_ != null) try { ctx_.close(); } catch (NamingException e) {}
 		}
 		System.exit(1);
 	}

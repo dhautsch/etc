f8fd0e6600d85593977b9d707311f6e55e143c30
dhautsch <don@hautsch.com>
2017-03-14 19:29:04 -0400

    Update JDBCExample.java

--- java/JDBCExample.java
+++ java/JDBCExample.java
@@ -1,57 +1,144 @@
 import java.sql.*;
 
 //
-// Following env vars and select is for netezza
-// https://www.tutorialspoint.com/jdbc/index.htm
-//
-// export JDBC_URL=jdbc:netezza://host:5480/TEST_DB
+// export JDBC_URL=jdbc:netezza://host:5480/TEST_DB?user=scott&password=tiger
 // export JDBC_DRIVER=org.netezza.Driver
-// export JDBC_USER=scott
-// export JDBC_PASS=tiger
+// export JDBC_URL=jdbc:oracle:thin:scott/tiger@//host:1521/SID
+// export JDBC_DRIVER=oracle.jdbc.OracleDriver
 // export JAVA_HOME=<PARENT_DIRS>/java
 // PATH=$JAVA_HOME/bin:$PATH
-// export CLASSPATH=<PARENT_DIRS>/nzjdbc3.jar:.
+// export CLASSPATH=<PARENT_DIRS>/nzjdbc3.jar:<PARENT_DIRS>/ojdbc6.jar:.
 // javac JDBCExample.java
 // java  JDBCExample
 //
 
-public class JDBCExample {
-	public static void main(String[] args) {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+
+class JDBCExample {
+	private static boolean isDateTimeColumn(int columnType) {
+		return (columnType == Types.TIMESTAMP) || (columnType == Types.DATE) || (columnType == Types.TIME);
+	}
+
+	public static void main(String args[]) {
 		Connection conn_ = null;
 		Statement stmt_ = null;
+		String colDelim_ = "|";
+
 		try {
-			// STEP 2: Register JDBC driver
+			boolean printUsage_ = false;
+			boolean getMeta_ = args.length > 0 && args[0].equals("--meta") ? true : false;
+			Path p_ = args.length > 1 ? Paths.get(args[1]) : null;
+			
+			printUsage_ = args.length < 2
+					|| (args[0].equals("--dump") == false && getMeta_ == false)
+					|| p_ == null
+					|| Files.isReadable(p_) == false;
+
+			if (printUsage_ == false && args.length > 2) {
+				try {
+					char c_ = (char) Integer.parseInt(args[2]);
+					colDelim_ = String.valueOf(c_);
+				} catch (NumberFormatException e_) {
+					printUsage_ = true;
+				}
+			}
+
+			if (printUsage_) {
+				System.out.println("Usage : " + JDBCExample.class.getName()
+						+ "(--dump|--meta) pathToSqlFile [colDelim lineDelim]");
+				System.out.println("\t--dump : data to stdout.");
+				System.out.println("\t--meta : metadata to stdout.");
+				System.out.println("pathToSqlFile : path to file containing sql.");
+				System.out.println("colDelim : optional character for column delimiter in decimal. Defaults to |.");
+				System.out.println();
+				System.out.println("export JDBC_DRIVER=org.netezza.Driver");
+				System.out.println("export JDBC_URL=jdbc:netezza://HOST:PORT/DB?user=USER&password=PASS");
+				System.out.println();
+				System.out.println("export JDBC_DRIVER=oracle.jdbc.OracleDriver");
+				System.out.println("export JDBC_URL=jdbc:oracle:thin:USER/PASS@//HOST:PORT/SID");
+				System.exit(0);
+			}
+
 			Class.forName(System.getenv("JDBC_DRIVER"));
 
-			// STEP 3: Open a connection
-			System.out.println("Connecting to database...");
-			conn_ = DriverManager.getConnection(System.getenv("JDBC_URL"), System.getenv("JDBC_USER"),
-					System.getenv("JDBC_PASS"));
+			conn_ = DriverManager.getConnection(System.getenv("JDBC_URL"));
 
-			// STEP 4: Execute a query
-			System.out.println("Creating statement...");
-			stmt_ = conn_.createStatement();
-			String sql_ = "select count(*) as cnt from _v_table";
-			ResultSet rs_ = stmt_.executeQuery(sql_);
-
-			// STEP 5: Extract data from result set
-			while (rs_.next()) {
-				// Retrieve by column name
-				int cnt_ = rs_.getInt("cnt");
-
-				// Display values
-				System.out.println(sql_ + ": " + cnt_);
+			for (SQLWarning warn = conn_.getWarnings(); warn != null; warn = warn.getNextWarning()) {
+				System.out.println("SQL Warning:");
+				System.out.println("State  : " + warn.getSQLState());
+				System.out.println("Message: " + warn.getMessage());
+				System.out.println("Error  : " + warn.getErrorCode());
 			}
-			// STEP 6: Clean-up environment
+
+			String sql_ = new String(Files.readAllBytes(p_));
+
+			stmt_ = conn_.createStatement();
+
+			sql_ = "SELECT t.* FROM (" + sql_.replace(";", "") + ") t";
+
+			if (getMeta_)
+				sql_ = sql_ + " WHERE 1 = 2";
+
+			ResultSet rs_ = stmt_.executeQuery(sql_);
+			ResultSetMetaData md_ = rs_.getMetaData();
+
+			if (getMeta_) {
+				System.out.println("{");
+				System.out.println("\t\042columns\042 : [");
+
+				for (int i = 1; i <= md_.getColumnCount(); i++) {
+					String name_ = md_.getColumnLabel(i);
+					String typeName_ = md_.getColumnTypeName(i);
+					int nullable_ = md_.isNullable(i);
+					int precision_ = md_.getPrecision(i);
+					int scale_ = md_.getScale(i);
+
+					System.out.print("\t\t");
+
+					if (i > 1)
+						System.out.print(",");
+
+					System.out.println("{");
+					System.out.println("\t\t\t\042name\042 : \042" + name_ + "\042,");
+					System.out.println("\t\t\t\042type\042 : \042" + typeName_ + "\042,");
+					System.out.println("\t\t\t\042precision\042 : \042" + precision_ + "\042,");
+					System.out.println("\t\t\t\042scale\042 : \042" + scale_ + "\042,");
+					System.out.println("\t\t\t\042nullable\042 : \042"
+							+ (nullable_ == DatabaseMetaData.columnNullable ? 1 : 0) + "\042");
+					System.out.println("\t\t}");
+				}
+				System.out.println("\t]");
+				System.out.println("}");
+			}
+
+			while (rs_.next()) {
+				for (int i_ = 1; i_ <= md_.getColumnCount(); i_++) {
+					System.out.print(rs_.getString(i_));
+					if (i_ < md_.getColumnCount())
+						System.out.print(colDelim_);
+				}
+				System.out.println();
+			}
+
+			// Close the result set, statement and the connection
 			rs_.close();
 			stmt_.close();
 			conn_.close();
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
-		} catch (Exception e) {
-			// Handle errors for Class.forName
-			e.printStackTrace();
+		} catch (SQLException se_) {
+			System.out.println("SQL Exception:");
+
+			while (se_ != null) {
+				System.out.println("State  : " + se_.getSQLState());
+				System.out.println("Message: " + se_.getMessage());
+				System.out.println("Error  : " + se_.getErrorCode());
+
+				se_ = se_.getNextException();
+			}
+		} catch (Exception e_) {
+			System.out.println(e_);
 		} finally {
 			// finally block used to close resources
 			try {
@@ -66,6 +153,5 @@ public class JDBCExample {
 				se.printStackTrace();
 			}
 		}
-		System.out.println("Goodbye!");
 	}
 }

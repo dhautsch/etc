49f6e9b595c6298de1565b10323e866a64648f54
dhautsch <don@hautsch.com>
2020-06-20 18:20:36 -0400

    Update ES_versions.js

--- javascript/ES_versions.js
+++ javascript/ES_versions.js
@@ -12,3 +12,46 @@ console.log(dragons.filter(s => s.includes('John')));
 
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
 const power100 = x => x**100;
+
+//
+// ES8(2017) adds padStart padEnd Object.values Object.entries
+//
+// #1) Line up the Turtle and the Rabbit at the start line:
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+
+// it should look like this:
+'     ||<- Start line'
+'       🐢'
+'       🐇'
+
+// when you do:
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+'     ||<- Start line'
+'🐢======='
+'       🐇'
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ')
